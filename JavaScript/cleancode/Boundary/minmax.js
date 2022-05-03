a9d452156c362c57e5940ce7039f6b6e02fa61be
jin <20220330jin@gmail.com>
2022-05-03 18:55:10 +0900

    [Javascript] min-max 표기

---
+++ JavaScript/cleancode/Boundary/minmax.js
@@ -0,0 +1,20 @@
+console.log('min-max');
+
+function getRandomNumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const MAX_AGE = 20;
+
+function isAdult(age){
+    if(age >= 20){
+
+    }
+}
+
+isAdult(MAX_AGE);
+
+const MIN_NUMBER = 1;
+const MAX_NUMBER = 45;
+
+getRandomNumber(MIN_NUMBER, MAX_NUMBER);
